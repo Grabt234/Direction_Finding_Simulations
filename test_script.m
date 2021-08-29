d24649389cd3d92f201204c83cc68b1030ab43bd
unknown <gnorrie12@gmail.com>
2021-08-29 13:10:05 +0200

    moving functions

--- test_script.m
+++ test_script.m
@@ -1,17 +1,8 @@
-%% 1
-
-%frequency of interest
-foi = 18e9;
-%linear array for x
-pos_elements = [0 0.0083 0.0125 0.025]; %meters
-
-
-[course_baseline, course_baseline_index] = select_baseline(pos_elements, foi);
-
-[fine_baseline, fine_baseline_index] = max(pos_elements);
 
 %% SIMULATING PHASE RECEIVAL
 
+%frequency of interest
+foi = 18e9;
 
 aoa = (35)*(pi/180);
 
@@ -20,6 +11,15 @@ aoa = (35)*(pi/180);
 %simulating receival phase 
 % ---- NORMALISED TO 2PI ----
 differential_phases = gen_diff_phases(pos_elements, foi, aoa)
+
+%linear array for x
+pos_elements = [0 0.0083 0.0125 0.025]; %meters
+
+%% SELECTING PORTION OF ARRAY TO USE
+
+[course_baseline, course_baseline_index] = select_baseline(pos_elements, foi);
+
+[fine_baseline, fine_baseline_index] = max(pos_elements);
 
 
 %% SELECTING DIFFERENTIAL PAHSES
